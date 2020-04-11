ec0ac39c0ae777d523a3dc8f8adae3d6d6c227d1
Samuele Carcagno <sam.carcagno@gmail.com>
2020-04-11 14:10:02 +0200

    switch to lualatex for pdf

--- build_gitbook.sh
+++ build_gitbook.sh
@@ -2,3 +2,5 @@
 
 Rscript -e "bookdown::render_book(input='index.Rmd', output_format='bookdown::gitbook', output_dir='../gitbook')"
 #Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+rm ../gitbook/rguide_datasets.zip
+zip ../gitbook/rguide_datasets.zip datasets/*
