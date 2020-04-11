89f8368f755740fc30da2f995150a30058415edc
Samuele Carcagno <sam.carcagno@gmail.com>
2020-04-11 19:20:52 +0200

    refactoring and updates

--- build_pdf.sh
+++ build_pdf.sh
@@ -2,3 +2,5 @@
 
 #Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 Rscript -e "bookdown::render_book(input='index.Rmd', output='bookdown::pdf_book', output_dir='../latex')"
+
+cp ../latex/rguide.pdf ../gitbook/rguide.pdf
